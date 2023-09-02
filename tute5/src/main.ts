a0c87134daaeb709d08fc2225a62bf43fa2ff7c8
yuanyi <wyuanyi1029@gmail.com>
2023-09-03 07:43:52 +0800

    tute 5 complete

--- tute5/src/main.ts
+++ tute5/src/main.ts
@@ -3,3 +3,10 @@
  *
  * Refer to the README and web page for instructions.
  */
+
+import { from, fromEvent } from "rxjs";
+import { map, filter, mergeMap } from "rxjs/operators";
+
+fromEvent(document, "click").pipe(
+    mergeMap
+)
